cf998133fd08900709d25ff2f580be5dcc2263c1
Joris Guien <joris.guien@avec.fr>
2025-09-09 10:36:43 +0200

    rollback config eslint

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { firstValueFrom } from 'rxjs';
